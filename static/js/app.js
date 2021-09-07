7b890ac815234009a30bbe835b6a0dbd37d538cf
Melisa Vacca <melisa.vacca@gmail.com>
2021-09-07 11:12:36 -0400

    updating app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,17 +4,16 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");
+  tbody.html("");
+  data.forEach((dataRow) => {
+  let row = tbody.append("tr");
+  Object.values(dataRow).forEach((val) => {
+    let cell = row.append("td");
+    cell.text(val);
+    }
+  );
+});
 }
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-      cell.text(val);
-      }
-    );
-  });
-
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
@@ -39,4 +38,4 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
-};
+;
